ff5b20bc7415637f5eaf9d5df8b8a91271f2756a
rberezen <ruslan.berezenskyi@gmail.com>
2022-05-21 18:36:29 +0300

    Initial setup

---
+++ src/test/java/com/berezenskyi/ruslan/MainPage.java
@@ -0,0 +1,78 @@
+package com.berezenskyi.ruslan;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class MainPage {
+    public WebDriver driver;
+
+    public MainPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+    @FindBy(id = "inputEmail")
+    public WebElement emailField;
+
+    @FindBy(id = "inputPassword")
+    public WebElement passwordField;
+
+    @FindBy(xpath = "//button[text()='Sign in']")
+    private WebElement signBtn;
+
+    @FindBy(id = "dropdownMenuButton")
+    public WebElement dropdownMenuButton;
+
+    @FindBy(xpath = "//div[@id='test-4-div']/button[1]")
+    public WebElement activeButton;
+
+    @FindBy(xpath = "//div[@id='test-4-div']/button[2]")
+    public WebElement disabledButton;
+
+    @FindBy(id = "test5-button")
+    public WebElement hiddenButton;
+
+    @FindBy(id = "test5-alert")
+    public WebElement alert;
+
+    public WebElement dropdownItem(String option) {
+        return driver.findElement(By.xpath(String.format("//a[@class='dropdown-item' and text()='%s']", option)));
+    }
+
+    public void inputEmail(String email) {
+        emailField.sendKeys(email);
+    }
+
+    public void inputPassword(String password) {
+        passwordField.sendKeys(password);
+    }
+
+    public void clickSignInBtn() {
+        signBtn.click();
+    }
+
+    public void clickDropdownMenuButton() {
+        dropdownMenuButton.click();
+    }
+
+    public WebElement getElementByCoordinates(int x, int y) {
+        List<WebElement> tableRows = driver.findElements(By.xpath("//div[@id='test-6-div']//tbody//tr"));
+        WebElement tableRow = tableRows.get(x);
+        List<WebElement> tableCells = tableRow.findElements(By.cssSelector("td"));
+        return tableCells.get(y);
+    }
+
+    public boolean isPresent(By locator) {
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (org.openqa.selenium.NoSuchElementException exception) {
+            return false;
+        }
+    }
+}
---
+++ src/test/java/com/berezenskyi/ruslan/SeleniumTest.java
@@ -0,0 +1,78 @@
+package com.berezenskyi.ruslan;
+
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class SeleniumTest {
+    public static MainPage mainPage;
+    public static WebDriver driver;
+
+    @BeforeClass
+    public static void setup() {
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + ConfProperties.getProperty("chromedriver"));
+        driver = new ChromeDriver();
+        mainPage = new MainPage(driver);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        driver.get("file:///" + System.getProperty("user.dir") + ConfProperties.getProperty("testpage"));
+    }
+
+    @Test
+    public void test1() {
+        Assert.assertTrue("Email field has not been found", mainPage.isPresent(By.id("inputEmail")));
+        Assert.assertTrue("Password field has not been found", mainPage.isPresent(By.id("inputPassword")));
+        Assert.assertTrue("Sign in button is not present", mainPage.isPresent(By.xpath("//button[text()='Sign in']")));
+        mainPage.inputEmail("email@com");
+        mainPage.inputPassword("password");
+        mainPage.clickSignInBtn();
+    }
+
+    @Test
+    public void test2() {
+        List<WebElement> allElements = driver.findElements(By.xpath("//ul[@class='list-group']/li"));
+        WebElement listItem2 = allElements.get(1);
+        Assert.assertEquals("There are not three values in the listgroup", 3, allElements.size());
+        Assert.assertTrue("Second list item's value is not set to List Item 2", listItem2.getText().contains("List Item 2"));
+        Assert.assertEquals("The second list item's badge value is not 6", "6", listItem2.findElement(By.className("badge")).getText());
+    }
+
+    @Test
+    public void test3() {
+        Assert.assertTrue("Option 1 is not the default selected value", mainPage.dropdownMenuButton.getText().contains("Option 1"));
+        mainPage.clickDropdownMenuButton();
+        mainPage.dropdownItem("Option 3").click();
+    }
+
+    @Test
+    public void test4() {
+        Assert.assertTrue("Button is disabled when expected to be enabled", mainPage.activeButton.isEnabled());
+        Assert.assertFalse("Button is active when expected to be disabled", mainPage.disabledButton.isEnabled());
+    }
+
+    @Test
+    public void test5() {
+        WebElement hiddenButton = new WebDriverWait(driver, 10)
+                .until(ExpectedConditions.visibilityOfElementLocated(By.id("test5-button")));
+        mainPage.hiddenButton.click();
+        Assert.assertFalse("Button is active when expected to be disabled", mainPage.hiddenButton.isEnabled());
+        Assert.assertTrue("Success message is wrong", mainPage.alert.getText().contains("You clicked a button!"));
+    }
+
+    @Test
+    public void test6() {
+        Assert.assertTrue("The value of the cell is not Ventosanzap", mainPage.getElementByCoordinates(2,2).getText().contains("Ventosanzap"));
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        driver.quit();
+    }
+}
